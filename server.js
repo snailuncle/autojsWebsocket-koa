f80ac13c227574f4d5ae98a3a2be04e579c030d1
snailuncle <1789500304@qq.com>
2019-05-08 12:42:24 +0800

    autojs-koa-websocket-传送字符串

--- server.js
+++ server.js
@@ -7,14 +7,31 @@ const route = require('koa-route')
 // koa封装的websocket这是官网（很简单有时间去看一下https://www.npmjs.com/package/koa-websocket）
 const websockify = require('koa-websocket')
 const app = websockify(new Koa());
+let ctxs = [];
 app.ws.use(function (ctx, next) {
-    return next(ctx)
+  ctxs.push(ctx);
+  return next(ctx)
 })
 app.ws.use(route.all('/', function (ctx) {
-    ctx.websocket.on('message', function (message) {
-        // 返回给前端的数据
-        ctx.websocket.send(message)
-    })
+  ctx.websocket.on('message', function (message) {
+    // 返回给前端的数据
+    if(message.length){
+      if(message.length<100){
+        console.log("服务端接收到字符串=" + message)
+      }else{
+        console.log("服务端接收到字符串长度=" + message.length)
+      }
+    }else{
+      console.log("服务端接收到" + message)
+    }
+
+    for (let i = 0; i < ctxs.length; i++) {
+      if (ctx == ctxs[i]) continue;
+      ctxs[i].websocket.send(message);
+    }
+    // ctx.websocket.send(message)
+  })
 }))
 app.listen(3000)
 // 会默认打开127.0.0.1:3000这个端口号
+
